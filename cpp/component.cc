e78f19ef9b626132c420ddde6bac033c231b81b8
Dragos Rachieru <rachierudragos97@gmail.com>
2023-01-28 21:25:00 +0200

    Started adding pin logic

---
+++ cpp/component.cc
@@ -0,0 +1,27 @@
+#include "component.hh"
+
+ComponentWithPins::ComponentWithPins(HalComponent component) : component(component)
+{
+}
+
+ComponentWithPins::~ComponentWithPins()
+{
+}
+
+HalPin ComponentWithPins::createPin(
+    std::string pin_id,
+    CreatePinRequest request)
+{
+
+    auto existing = pins.find(pin_id);
+    if (existing != pins.end())
+    {
+        return existing->second;
+    }
+    HalPin pin;
+
+    //    pin.dir = request.dir();
+
+    pin.set_component_id(component.component_id());
+    pin.set_component_name(component.name());
+}
